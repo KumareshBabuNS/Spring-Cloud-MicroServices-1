a20572a4067c4208ceb7cab4f7554a1f667b04fe
张志豪 <izhangzhihao@hotmail.com>
2016-09-17 14:53:03 +0800

    使用Feign实现简单负载均衡

---
+++ FeignLoadBalance/src/main/java/com/github/izhangzhihao/FeignLoadBalance/ConsumerController.java
@@ -0,0 +1,17 @@
+package com.github.izhangzhihao.FeignLoadBalance;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    ComputeClient computeClient;
+
+    @GetMapping("/add")
+    public Integer add() {
+        return computeClient.add(10, 20);
+    }
+}
