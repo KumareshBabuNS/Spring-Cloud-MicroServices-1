4d1638644a849f831c4a3882a8cfb2fd81ad4405
张志豪 <izhangzhihao@hotmail.com>
2016-09-17 20:38:44 +0800

    为 Ribbon 添加 hystrix

--- RibbonLoadBalance/src/main/java/com/github/izhangzhihao/SpringCloud/RibbonLoadBalance/ConsumerController.java
+++ RibbonLoadBalance/src/main/java/com/github/izhangzhihao/SpringCloud/RibbonLoadBalance/ConsumerController.java
@@ -3,15 +3,18 @@ package com.github.izhangzhihao.SpringCloud.RibbonLoadBalance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ConsumerController {
+//    @Autowired
+//    RestTemplate restTemplate;
+
     @Autowired
-    RestTemplate restTemplate;
+    private ComputeService computeService;
 
     @GetMapping("/add")
     public String add() {
-        return restTemplate.getForEntity("http://COMPUTE-SERVICE/add?a=10&b=20", String.class).getBody();
+        //return restTemplate.getForEntity("http://COMPUTE-SERVICE/add?a=10&b=20", String.class).getBody();
+        return computeService.addService();
     }
 }
