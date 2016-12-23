7007aed982b6f11605b7710d1b472f7a51e06422
张志豪 <izhangzhihao@hotmail.com>
2016-12-23 20:19:41 +0800

    enable Hystrix Dashboard

--- LoadBalance-Service/src/main/java/com/github/izhangzhihao/LoadBalanceService/Application.java
+++ LoadBalance-Service/src/main/java/com/github/izhangzhihao/LoadBalanceService/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrix //expose stream
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -21,6 +23,6 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "forward:/hystrix";
+        return "forward:/hystrix.stream";
     }
 }
